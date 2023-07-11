c97dd870a8b40da71314ca38fb8fb38bcea34424
Mark Miller <Mark.Miller1@monash.edu>
2023-07-11 17:58:42 +1000

    starting data explore

--- tracking_preprocess.R
+++ tracking_preprocess.R
@@ -1,1 +1,39 @@
 # data explore clean
+
+library(ggplot2)
+library(lubridate)
+library(dplyr)
+library(GGally)
+
+setwd("C:/Users/mmil0049/OneDrive - Monash University/projects/02 flight heights/data/shy_albatross_island")
+
+# Combine data from two tags that uploaded data while on birds
+
+dat<-rbind(read.csv("gps_89460800120108611854.csv"),read.csv("gps_89460800120141490936.csv"))
+table(dat$iccid)
+
+dat$DateTime<-ymd_hms(dat$time_UTC, tz="UTC")
+dat$DateTime_AEDT<-with_tz(dat$DateTime, "Australia/Sydney")
+
+# positional errors
+ggplot(data=dat)+geom_point(aes(x=lon, y=lat))
+dat[dat$lon==0,] # 3 of them but don't remove as valid pressure and temp
+ggplot(data=filter(dat, lat<0))+geom_point(aes(x=lon, y=lat, colour=factor(iccid)))
+
+#filter time to after deployment on each bird
+# 854 out 23-04-01 17:12:00
+# 936 out 23-04-01 17:59:00
+
+# frequency
+ggplot(data=dat)+
+  geom_point(aes(x=DateTime_AEDT, y=temp))+
+  geom_vline(xintercept=ymd_hms("23-04-01 17:59:00", tz="Australia/Sydney"))+
+  facet_wrap(~iccid)
+
+# lots of variation in temp.. maybe correlated with solar heating?
+
+ggpairs(data=dat%>%filter(DateTime_AEDT>ymd_hms("23-04-01 17:59:00", tz="Australia/Sydney")),
+        columns=c("DateTime_AEDT", "lat", "iSolar", "alt", "temp",
+                            "pres_pa"), ggplot2::aes(colour=factor(iccid)))
+
+# add dist colony. Actually use track2kba to get masic metrics and split trips
